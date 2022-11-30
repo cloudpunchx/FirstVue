597b1366bf8d2a079fe979561b3a3466b1c4a8bd
cloudpunchx <siobhanbonardi@hotmail.com>
2022-11-29 20:48:58 -0700

    Started first VueJS application.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
+<!-- DECLARE, REGISTER, USE (COMPONENTS) -->
+
 <template>
+  <!-- THIS IS THE ONLY ID ALLOWED IN VUE.JS DO NOT USE THEM ANYWHERE ELSE -->
+  <!-- WHEN IT 'MOUNTS' THIS APP ID IS WHAT IT LOOKS FOR, ITS IN THE MAIN.JS -->
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <!-- 3. USE -->
+    <!-- he recommends sticking to the casing, so just use camel case. -->
+    <!-- its written like this because its a tag being translated and doesn't need a closing tag -->
+    <!-- and we aren't putting content here since we already did it INSIDE the component -->
+    <MyComponent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// 1. DECLARE
+// the @ is equivalent to stating the rest of the folders listed
+import MyComponent from '@/components/MyComponent.vue';
 
 export default {
   name: 'App',
+  // add components and list them
   components: {
-    HelloWorld
+    // 2. REGISTER
+    MyComponent
   }
 }
 </script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<!-- 3 STEPS FOR COMPONENTS SEE BELOW -->
+
+<!-- used vbase-css shortcut for this, could also choose SCSS instead if I want to use SCSS -->
+<template>
+    <div>
+        <h1>Hello Components!</h1>
+    </div>
+</template>
+
+<script>
+// This is what allows us to use this component in other places
+// Mark said he never likes to use back ticks because they can mess things up
+    export default {
+        name : "MyComponent",
+    }
+</script>
+
+<style scoped>
+
+</style>
